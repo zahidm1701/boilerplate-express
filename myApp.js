34497adad78f2966eb27940d4fd3eba032e90258
Zahid <101910891+zahidm1701@users.noreply.github.com>
2026-01-06 19:46:26 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ console.log("Hello World");
 
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.sendFile(__dirname + "/views/index.html"));
 });
 
 
