b8080563e9b44889f2b4b1f00d849e6ad3cb7b19
Zahid <101910891+zahidm1701@users.noreply.github.com>
2026-01-06 19:31:06 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,8 +5,8 @@ let app = express();
 console.log("Hello World");
 
 
-app.get("/", function (req, res) {
- res.send("Hello Express"):
+app.get("/", function(req, res) {
+  res.send("Hello Express");
 });
 
 
