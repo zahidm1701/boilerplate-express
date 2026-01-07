b76578ad20e9c92cdbb0d3f3fc4992b7a19acd3d
Zahid <101910891+zahidm1701@users.noreply.github.com>
2026-01-07 23:59:37 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -15,6 +15,11 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
 
 
 
