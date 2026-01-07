f3fd2550daad045982d7fe5a7ff8f3bc94054abb
Zahid <101910891+zahidm1701@users.noreply.github.com>
2026-01-07 23:49:23 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,7 +4,10 @@ let app = express();
 
 console.log("Hello World");
 
+// Serve static files from /public
+app.use(express.static(__dirname + "/public"));
 
+// Serve HTML file
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
